9ec7ec07afa2af43ddd42dc2835031814fd95d21
Ovidiu CONEAC <ovidiu.coneac@gmail.com>
2017-04-22 00:16:51 +0300

    Initla commit

--- src/main/java/ro/ovidiuconeac/foodtavern/Main.java
+++ src/main/java/ro/ovidiuconeac/foodtavern/Main.java
@@ -1,10 +1,7 @@
 package ro.ovidiuconeac.foodtavern;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Created by Ovidiu CONEAC on 2/12/2017.
